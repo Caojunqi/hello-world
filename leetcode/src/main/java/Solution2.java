7f75c9d66860c23a43dc4ae28424fb496e3651b2
caojunqi <caojunqi@foxmail.com>
2020-01-03 10:15:52 +0800

    2题解答优化

--- leetcode/src/main/java/Solution2.java
+++ leetcode/src/main/java/Solution2.java
@@ -1,13 +1,44 @@
+/**
+ * 2.两数相加
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class Solution2 {
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        l1.appendValue(4);
+        ListNode l1 = new ListNode(1);
+        l1.appendValue(6);
+        l1.appendValue(0);
         l1.appendValue(3);
-        l1.appendValue(5);
+        l1.appendValue(3);
+        l1.appendValue(6);
+        l1.appendValue(7);
+        l1.appendValue(2);
+        l1.appendValue(0);
+        l1.appendValue(1);
         printListNode(l1);
-        ListNode l2 = new ListNode(5);
+        ListNode l2 = new ListNode(6);
+        l2.appendValue(3);
+        l2.appendValue(0);
+        l2.appendValue(8);
+        l2.appendValue(9);
         l2.appendValue(6);
-        l2.appendValue(4);
+        l2.appendValue(6);
+        l2.appendValue(9);
+        l2.appendValue(6);
+        l2.appendValue(1);
         printListNode(l2);
 
         printListNode(addTwoNumbers(l1, l2));
@@ -18,13 +49,14 @@ public class Solution2 {
     }
 
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2, int inheritValue) {
+        if (l1 == null && l2 == null && inheritValue == 0) {
+            return null;
+        }
+
         int value1 = l1 == null ? 0 : l1.value;
         int value2 = l2 == null ? 0 : l2.value;
 
         int totalValue = value1 + value2 + inheritValue;
-        if (totalValue == 0) {
-            return null;
-        }
         int nodeValue = totalValue % 10;
         inheritValue = totalValue / 10;
 
@@ -32,7 +64,6 @@ public class Solution2 {
         ListNode nextL1 = l1 == null ? null : l1.next;
         ListNode nextL2 = l2 == null ? null : l2.next;
         if (nextL1 != null || nextL2 != null || inheritValue != 0) {
-
             ListNode next = addTwoNumbers(nextL1, nextL2, inheritValue);
             if (next != null) {
                 result.setNext(next);
